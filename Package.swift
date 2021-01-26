4a21f32b2038177288151cf212384028224b8209
softwareolatomiwa <softwareolatomiwa@hotmail.com>
2021-01-26 11:53:03 +0100

    Added changes to Package.swift

--- Package.swift
+++ Package.swift
@@ -5,10 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "EpumpFramework",
+    platforms: [.iOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "EpumpFramework",
+            type: .dynamic,
             targets: ["EpumpFramework"]),
     ],
     dependencies: [
